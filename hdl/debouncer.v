dd5df161c7105bd7faeddfacfd02e3ddbf7ed8f5
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-11-14 02:53:29 +0100

    Update debouncer.v
    removing comments

--- hdl/debouncer.v
+++ hdl/debouncer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.10.2025 22:52:38
-// Design Name: 
-// Module Name: debounce
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module debouncer(
         input btn,
