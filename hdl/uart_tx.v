7bf322075c16056c0d6b98cd4ed9590cfc28ea19
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-12-01 16:47:09 +0100

    Clean up header comments in uart_tx.v
    Removed header comments from uart_tx.v

--- hdl/uart_tx.v
+++ hdl/uart_tx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.10.2025 19:17:20
-// Design Name: 
-// Module Name: uart_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_tx #(
         parameter CLK_FREQ = 100_000_000, 
