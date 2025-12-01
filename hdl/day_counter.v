ec74f12e7c8b724edf238d0ec9805058d93ec5ce
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-12-01 16:36:50 +0100

    Refactor day_counter module for clarity and efficiency

--- hdl/day_counter.v
+++ hdl/day_counter.v
@@ -1,19 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.10.2025
-// Design Name: 
-// Module Name: day_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: BCD day counter
-//////////////////////////////////////////////////////////////////////////////////
 
-
-// File: day_counter.v
 module day_counter(
         input clk,
         input rst,
@@ -21,17 +7,16 @@ module day_counter(
         input [3:0] month_tens,
         input [3:0] month_units,input load_en,
         input [4:0] load_day,
-        output reg [3:0] du,   // nap egységek
-        output reg [3:0] dt,   // nap tízesek
-        output reg cout         // jelez, ha új hónap kezdõdik
+        output reg [3:0] du,   
+        output reg [3:0] dt,   
+        output reg cout        
     );
     
-    reg [4:0] max_days; // hónap hossz, max 31
-    wire [5:0] current_day; // BCD tízes + egység
+    reg [4:0] max_days; 
+    wire [5:0] current_day; 
 
     assign current_day = dt*10 + du;
 
-    // Hónap szerint napok száma
     always @(*)
         case({month_tens, month_units})
             8'h01, 8'h03, 8'h05, 8'h07, 8'h08, 8'h10, 8'h12: max_days = 31;
@@ -39,7 +24,6 @@ module day_counter(
             8'h02: max_days = 28; 
         endcase
 
-    // Nap számlálás
     always @(posedge clk) 
         begin
             if (rst) 
@@ -60,7 +44,7 @@ module day_counter(
                         begin
                             du <= 4'd1;
                             dt <= 4'd0;
-                            cout <= 1; // új hónap kezdõdik
+                            cout <= 1;
                         end
                     else
                         begin
