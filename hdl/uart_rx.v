beca3b2c0315d7f303ea79c784dbda5b71721d0a
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-12-01 16:46:49 +0100

    Clean up comments in uart_rx.v
    Removed header comments from uart_rx.v

--- hdl/uart_rx.v
+++ hdl/uart_rx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.10.2025 19:18:07
-// Design Name: 
-// Module Name: uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_rx #(
             parameter CLK_FREQ = 100_000_000,
