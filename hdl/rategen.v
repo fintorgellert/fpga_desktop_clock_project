3522498e2bdc323e5be1d81bc2745b398b5d6e0a
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-12-01 16:39:06 +0100

    Refactor rategen.v by cleaning up comments
    Removed unnecessary comments and cleaned up the header.

--- hdl/rategen.v
+++ hdl/rategen.v
@@ -1,31 +1,11 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.10.2025 14:04:44
-// Design Name: 
-// Module Name: rategen
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-// File: rategen.v
 module rategen(
         input clk, rst,  
         output cy
     );
 
-    reg [29:0] Q; // 100MHz -> 1Hz (27 bit elég: 100e6 < 2^27)
+    reg [29:0] Q; // 100MHz -> 1Hz 
     
     always @(posedge clk) 
         begin 
